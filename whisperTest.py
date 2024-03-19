28e65d367909893289de9e269882ab8aa9232a22
ChakirKharraz <125362489+ChakirKharraz@users.noreply.github.com>
2024-03-19 23:46:11 +0100

    Whisper.py using OpenAi API
    Fichier python dans lequel on met une key APi ainsi qu'un path avec le fichier a retranscrire et ca nous output un fichier text a un path donné avec toute la retranscription.

---
+++ whisperTest.py
@@ -0,0 +1,21 @@
+from openai import OpenAI
+
+api_key = "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"  # Replace this with your actual API key
+client = OpenAI(api_key=api_key)
+
+with open("/home/kali/Desktop/AncientJapanesePoem.mp4", "rb") as audio_file:
+    transcription = client.audio.transcriptions.create(
+        model="whisper-1",
+        file=audio_file
+    )
+    # Get the transcription text
+    transcription_text = transcription.text
+
+# Define the path for the output text file
+output_file_path = "/home/kali/Desktop/Transcription.txt"
+
+# Write the transcription text to the output file
+with open(output_file_path, "w") as output_file:
+    output_file.write(transcription_text)
+
+print("Transcription saved to:", output_file_path)
