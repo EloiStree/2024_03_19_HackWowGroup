73d656be989f7ce2f4ba63a97019f32dd83245fb
ChakirKharraz <125362489+ChakirKharraz@users.noreply.github.com>
2024-03-19 23:55:57 +0100

    Translation.py using OpenAi API
    Meme fonctionnement que pour le whisper mais ici ca traduit tout les texte de la vidéo en anglais (open ai ne propose pas encore d'autre langue uniquement la traduction vers l'anglais est possible)

---
+++ translation.py
@@ -0,0 +1,20 @@
+from openai import OpenAI
+
+api_key = "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"  # Replace this with your actual API key
+client = OpenAI(api_key=api_key)
+
+audio_file= open("/home/kali/Desktop/atoutalheure.mp3", "rb")
+translation = client.audio.translations.create(
+  model="whisper-1", 
+  file=audio_file
+)
+translation_text = translation.text
+
+# Define the path for the output text file
+output_file_path = "/home/kali/Desktop/translation.txt"
+
+# Write the transcription text to the output file
+with open(output_file_path, "w") as output_file:
+    output_file.write(translation_text)
+
+print("Translation saved to:", output_file_path)
